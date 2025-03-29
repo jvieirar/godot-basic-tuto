813127da1cfb1caad42815d6623ce96eedccaf74
Juan Vieira <jmvieirar@gmail.com>
2025-03-29 16:55:54 +1100

    win label update

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,6 +2,9 @@ extends Node
 
 var score := 0
 
+@onready var label_end_level_score: Label = $LabelEndLevelScore
+
 func add_point():
 	score += 1
-	print("score: " + str(score))
+	label_end_level_score.text = "You collected " + str(score) + " coins!"
+	print(label_end_level_score.text)
