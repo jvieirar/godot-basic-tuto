ea14fe44c640ba3ea8995927bf219e00c8998d8e
Juan Vieira <jmvieirar@gmail.com>
2025-03-29 16:26:04 +1100

    fall to death

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -2,11 +2,17 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
-func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	print("you died!!")
-	timer.start()
-
-
 func _on_timer_timeout() -> void:
 	# restart the game
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("you died!!")
+	Engine.time_scale = 0.5
+	
+	# body is our player (only thing that collides with killing zone
+	body.get_node("CollisionShape2D").queue_free()
+	
+	timer.start()
