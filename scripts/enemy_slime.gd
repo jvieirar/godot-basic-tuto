456a4861975e7106d1ea959f29c794cdfd9ae8b7
Juan Vieira <jmvieirar@gmail.com>
2025-03-29 16:20:42 +1100

    raycast enemy

---
+++ scripts/enemy_slime.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const SPEED = 80
+
+var direction = 1
+
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if ray_cast_left.is_colliding():
+		direction = 1
+	if ray_cast_right.is_colliding():
+		direction = -1
+	
+	# need delta to compensate the speed addition
+	position.x += direction * SPEED * delta
