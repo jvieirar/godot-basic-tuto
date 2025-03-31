594f541e2e98bf453bb174370178b9a5846bbb12
Juan Vieira <jmvieirar@gmail.com>
2025-03-31 22:32:44 +1100

    new world building

--- scripts/game.gd
+++ scripts/game.gd
@@ -23,6 +23,10 @@ func _ready():
 	swap_cooldown.autostart = false
 	add_child(swap_cooldown)
 	swap_cooldown.timeout.connect(_on_swap_cooldown_timeout)
+	
+	# Window size
+	_set_window_size()
+	
 
 func _on_player_collision(_other_player):
 	if game_manager and can_swap:
@@ -32,3 +36,25 @@ func _on_player_collision(_other_player):
 
 func _on_swap_cooldown_timeout():
 	can_swap = true
+
+func _set_window_size():
+	# Get the screen size
+	var screen_size = DisplayServer.screen_get_size()
+	
+	# Calculate window size (e.g., 80% of screen width)
+	var scale_factor = 0.5  # 80% of screen size
+	var window_width = int(screen_size.x * scale_factor)
+	# Calculate height to maintain 16:9 ratio
+	var window_height = int(window_width * 9.0 / 16.0)
+	
+	# Make sure the height doesn't exceed screen height
+	if window_height > screen_size.y:
+		window_height = int(screen_size.y * scale_factor)
+		window_width = int(window_height * 16.0 / 9.0)
+	
+	# Set the window size
+	DisplayServer.window_set_size(Vector2i(window_width, window_height))
+	
+	# Center the window
+	var centered_pos = (screen_size - Vector2i(window_width, window_height)) / 2
+	DisplayServer.window_set_position(centered_pos)
